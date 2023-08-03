f6005bcfd374455f7da4fd5c63b4f1d6e8e50c62
dem0N4k <demON4k1993@gmail.com>
2023-08-04 01:52:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*System.out.println("Калькулятор умножения");
+        System.out.println("Введите первое число:");
+        int xMult = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int yMult = new Scanner(System.in).nextInt();
+        System.out.println("Число " + xMult + " * " + "число " + yMult + " = " +
+                (xMult * yMult) + " !");*/
+
+        /* System.out.println("Калькулятор суммы чисел");
+        System.out.println("Enter the first number:");
+        double xAdd = new Scanner(System.in).nextDouble();
+        System.out.println("Enter the second number:");
+        double yAdd = new Scanner(System.in).nextDouble();
+        System.out.println("Число " + xAdd + " + " + "число " + yAdd + " = " +
+                (xAdd + yAdd) + " !");*/
+
+        /*System.out.println("Калькулятор разности чисел");
+        System.out.println("Enter the first number:");
+        double xRes = new Scanner(System.in).nextDouble();
+        System.out.println("Enter the second number:");
+        double yRes = new Scanner(System.in).nextDouble();
+        System.out.println("Число " + xRes + " - " + "число " + yRes + " = " +
+                (xRes - yRes) + " !");*/
+
+        System.out.println("Калькулятор деления");
+        System.out.println("Enter the first number:");
+        double xQout = new Scanner(System.in).nextDouble();
+        System.out.println("Enter the second number:");
+        double yQout = new Scanner(System.in).nextDouble();
+        System.out.println("Число " + xQout + " / " + "число " + yQout + " = " +
+                (xQout / yQout) + " !");
+
+    }
+}
